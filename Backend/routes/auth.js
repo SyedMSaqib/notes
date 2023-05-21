cf5dda2c7f5090e02352d1c6cd49aa86855fb6b8
Saqib <splasher733@gmail.com>
2023-05-21 21:06:22 +0500

    Added express validation on email, name and password

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,13 +1,21 @@
 const express= require("express")
 const router=express.Router()
 const User=require("../models/User")
-
-router.post("/api/auth",(req,res)=>{
-  const user=User(req.body)
-   user.save()
-
-   res.send(req.body)
-
+const { check, validationResult } = require('express-validator');
+router.post("/api/auth",[
+  check('email').isEmail(),
+  check('password').isLength({ min: 6 })]
+  ,(req,res)=>{
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.json(result)
+    }
+    else
+   {
+    const user=User(req.body)
+    user.save()
+  }
+  
     
 })
 module.exports=router
