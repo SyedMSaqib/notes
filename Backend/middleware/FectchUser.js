25e62363a363017a61ff8c71743b20327ce90afe
Saqib <splasher733@gmail.com>
2023-05-24 03:34:36 +0500

    nothinggggg

--- Backend/middleware/FectchUser.js
+++ Backend/middleware/FectchUser.js
@@ -5,7 +5,6 @@ const key=require("../routes/auth")
 const fetchUser = (req, res, next) => {
     const token=req.header("auth-token")
     const verifyToken=jwt.verify(token, "02192000");
-    console.log(verifyToken)
     if(!verifyToken)
     return req.status(401).json({"Error":"Empty token"})
     try {
