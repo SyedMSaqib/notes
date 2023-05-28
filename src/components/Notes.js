ff796d10fc84ea12cdc65534940b79ec0cd9ee1f
Saqib <splasher733@gmail.com>
2023-05-28 18:36:27 +0500

    notesItem for containing data of notes and notes that will contain noteItem added, and home is containing the notes

---
+++ src/components/Notes.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useContext } from "react"
+import NotesContext from "../context/notes/NotesContext"
+import NoteItem from './NoteItem'
+
+
+const Notes = () => {
+    const {notes,setNotes}=useContext(NotesContext)
+
+  return (
+    <div className='row'>
+    <h2 className="my-3">Your notes</h2>
+       {
+        notes.map((data)=>{
+            return <NoteItem notes={data}/>
+        })
+       }
+
+        </div>
+  )
+}
+
+export default Notes
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,14 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 import context from './NotesContext'
 
 
 const NoteState = (props) => {
-    const state={
-        "name":"saqib",
-        "sem": "7"
+    const notesDb=[{
+        _id: "646e56d65d8f7be3f4a89ccd",
+        user: "646e56c35d8f7be3f4a89ccb",
+        title: "Name",
+        description: " Hello, my name is Saqib",
+        tag: "personal",
+        date: "1684952790028",
+        __v: 0
     }
+        ,
+    {
+        "_id": "6471c36673b2389cecbfa3cd",
+        "user": "646e56c35d8f7be3f4a89ccb",
+        "title": "Name",
+        "description": " Hello, my name is Ali",
+        "tag": "personal",
+        "date": "1685177190279",
+        "__v": 0
+      },
+      {
+        _id: "646e56d65d8f7be3f4a89ccd",
+        user: "646e56c35d8f7be3f4a89ccb",
+        title: "Name",
+        description: " Hello, my name is Saqib",
+        tag: "personal",
+        date: "1684952790028",
+        __v: 0
+    }
+        ,
+        {
+            _id: "646e56d65d8f7be3f4a89ccd",
+            user: "646e56c35d8f7be3f4a89ccb",
+            title: "Name",
+            description: " Hello, my name is Saqib",
+            tag: "personal",
+            date: "1684952790028",
+            __v: 0
+        }
+            ,
+            {
+                _id: "646e56d65d8f7be3f4a89ccd",
+                user: "646e56c35d8f7be3f4a89ccb",
+                title: "Name",
+                description: " Hello, my name is Saqib",
+                tag: "personal",
+                date: "1684952790028",
+                __v: 0
+            }
+                
+    
+    ]
+    
+      const [notes, setnotes] = useState(notesDb)
   return (
-    <context.Provider value={state}>
+    <context.Provider value={{notes,setnotes}}>
         {props.children}
     </context.Provider>
   )
